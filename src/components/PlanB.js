310e597211084a882c405c5fceae0558f5131ec3
bpotts11 <brittneympotts@gmail.com>
2021-06-18 16:39:48 -0500

    feat: logout button working, basic nav bar

--- src/components/PlanB.js
+++ src/components/PlanB.js
@@ -1,16 +1,17 @@
 import React from "react"
 import { Route, Redirect } from "react-router-dom"
 import { ApplicationViews } from "./ApplicationViews"
-// import { NavBar } from "./nav/NavBar"
+import { NavBar } from "./nav/NavBar"
 import { Login } from "./auth/Login"
 import { VendorRegister } from "./auth/VendorRegister"
 import { CustomerRegister } from "./auth/CustomerRegister"
 import { Register } from "./auth/Register"
+import { Logout } from "./auth/Logout"
 
 export const PlanB = () => (
     <>
-        {/* <Route render={() => {
-            if (localStorage.getItem("planb_id")) {
+        <Route render={() => {
+            if (localStorage.getItem("planb_customerId") || localStorage.getItem("planb_vendorId")) {
                 return <>
                     <NavBar />
                     <ApplicationViews />
@@ -18,7 +19,7 @@ export const PlanB = () => (
             } else {
                 return <Redirect to="/login" />
             }
-        }} /> */}
+        }} />
 
         <Route path="/login" render={() => {
             if (localStorage.getItem("planb_id")) {
@@ -32,19 +33,17 @@ export const PlanB = () => (
             <Register />
         </Route>
 
-        <Route path="/vendorregister" render={() => {
-            if (localStorage.getItem("planb_id")) {
-                return <Redirect to="/" />
-            } else {
-                return <VendorRegister />
-            }
-        }} />
-        <Route path="/customerregister" render={() => {
-            if (localStorage.getItem("planb_id")) {
-                return <Redirect to="/" />
-            } else {
-                return <CustomerRegister />
-            }
-        }} />
+        <Route path="/customerregister">
+            <CustomerRegister />
+        </Route>
+
+        <Route path="/vendorregister">
+            <VendorRegister />
+        </Route>
+
+        <Route path="/logout">
+            <Logout />
+        </Route>
     </>
+
 )
